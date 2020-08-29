49faa7532922eea6e239754f4050855ffa9f451b
Limo <liam@acordsoftware.tech>
2020-08-29 10:27:40 +0800

    Making an interactive component
    Clickup: CU-26pthc
    
    https://www.notion.so/Javascript-NPM-4c98f6c8a3304cc98da243f32ea6a8a0
    
    https: //www.notion.so/React-Tutorial-1-5c117f33003a4e819575123e1c238c22
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ class Square extends React.Component {
   render() {
     return ( // Create a button with the class name "square". This is to be completed 
       <button className="square">  
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,8 +14,9 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
+}
 
   render() {
     const status = 'Next player: X';
