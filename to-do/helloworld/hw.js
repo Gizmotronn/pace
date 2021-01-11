a68b6973791d602dc06db923d88ecd7df38160b1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-11 08:35:25 +0000

    https://www.notion.so/skinetics/React-Documentation-cee06c16ee0e4b8badcce7044b8019a8

---
+++ to-do/helloworld/hw.js
@@ -0,0 +1,7 @@
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+	element,
+	document.getElementById('root')
+);
---
+++ to-do/helloworld/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    const name = 'Josh Perez';
+    const element = <h1>Hello, {name}</h1>;
+    
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  );
+}
+
+export default App;
